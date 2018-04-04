b4350a8b5105ea8c4a5ef8db47c44324a6dee209
rich costello <richcostello68@gmail.com>
2018-04-03 19:54:41 -0700

    Edit to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,8 @@ class HomePage extends React.Component {
      render() {
          return (
              <div>
-             This is the HomePage Dude!
+             This is the HomePage Dude!<br>
+             Click on a hex to read it.
              </div>
          );
      }
